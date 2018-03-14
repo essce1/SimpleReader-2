1400ca346de534ed3500b50020cf35cc904e93be
Jakob C <jakobce@gmail.com>
2018-03-14 15:40:01 +0100

    Initial Commit

---
+++ SimpleReader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleReader
+//
+//  Created by Jakob Cederlund on 2018-03-14.
+//  Copyright © 2018 dev4phone. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
